a6ec994353d0ead49c55d6661c75d7605e09694d
Loz Jackson <lozjackson@talktalk.net>
2016-08-19 13:30:05 +0100

    update mask-input

--- addon/components/mask-input.js
+++ addon/components/mask-input.js
@@ -1,13 +1,12 @@
 import Ember from 'ember';
-// import TextMask from 'ember-text-mask/lib/text-mask';
-import TextMask from 'text-mask-core';
+import { createTextMaskInputElement } from 'text-mask-core';
 
 export default Ember.TextField.extend({
 
   didInsertElement() {
     this._super(...arguments);
     let { element:inputElement, mask } = this.getProperties('element', 'mask');
-    this.set('textMaskInputElement', TextMask.textMaskCore.createTextMaskInputElement({ inputElement, mask }));
+    this.set('textMaskInputElement', createTextMaskInputElement({ inputElement, mask }));
   },
 
   input(event) {
